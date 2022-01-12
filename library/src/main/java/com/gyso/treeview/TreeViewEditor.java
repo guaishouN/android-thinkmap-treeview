23c93d7dee5252d9bf10618f0a29753540fa9a1d
Guihui.Deng <Guihui.Deng@desay-svautomotive.com>
2022-01-12 17:41:17 +0800

    Revert "function list"
    This reverts commit edbe86f99067298049d4bf7c6efc45a5ebf79e4d.

--- library/src/main/java/com/gyso/treeview/TreeViewEditor.java
+++ library/src/main/java/com/gyso/treeview/TreeViewEditor.java
@@ -1,20 +1,16 @@
 package com.gyso.treeview;
 
-import android.view.View;
-
 import com.gyso.treeview.adapter.TreeViewAdapter;
 import com.gyso.treeview.model.NodeModel;
 
-import java.io.File;
 import java.lang.ref.WeakReference;
-import java.util.List;
 
 /**
  * @Author: 怪兽N
  * @Time: 2021/6/9  15:31
  * @Email: 674149099@qq.com
  * @WeChat: guaishouN
- * @Describe: Android developer
+ * @Describe:
  *
  * helper you edit your tree view.
  * Move node by dragging and remove node is support now.
@@ -30,11 +26,9 @@ public class TreeViewEditor {
         this.containerWeakReference = new WeakReference<>(container);
         this.adapterWeakReference = new WeakReference<>(container.getAdapter());
     }
-
     private TreeViewContainer getContainer(){
         return containerWeakReference.get();
     }
-
     private TreeViewAdapter<?> getAdapter(){
         return adapterWeakReference.get();
     }
@@ -44,122 +38,6 @@ public class TreeViewEditor {
     public void focusMidLocation(){
         TreeViewContainer container = getContainer();
         if (container!=null)container.focusMidLocation();
-    }
-
-    public View anchorNodeOnMidViewport(NodeModel<?> targetNode){
-        //TODO move targetNode at center  of viewport
-        return null;
-    }
-
-    /**
-     *  change layout algorithm
-     */
-    public void changeLayoutAlgorithm(){
-    }
-
-    /**
-     *  get current relation ship, you can use when you change
-     * @param traverse relations node
-     */
-    public<T> void getCurrentRelationships(TraverseRelationshipCallback traverse){
-
-    }
-
-    /**
-     *  get current relation ship, you can use when you change
-     * @param traverse relations node
-     * @param jsonStringFilePath jsonStringFilePath
-     */
-    public boolean save(String jsonStringFilePath, TraverseRelationshipCallback traverse){
-            return  false;
-    }
-
-    public boolean save(File jsonStringFile, TraverseRelationshipCallback traverse){
-        return  false;
-    }
-
-    /**
-     * load data  from json String
-     * @param jsonString string
-     */
-    public void load(String jsonString){
-
-    }
-
-    /**
-     * load data  from file
-     * @param jsonStringFile file
-     */
-    public void load(File jsonStringFile){
-
-    }
-
-    /**
-     *  expand by node
-     * @param targetParentNode targetParentNode
-     */
-    public void collapse(NodeModel<?> targetParentNode){
-
-    }
-
-    /**
-     *  expand by node
-     * @param targetParentNode targetParentNode
-     */
-    public void expand(NodeModel<?> targetParentNode){
-
-    }
-
-    /**
-     *  focus on node
-     * @param targetNode targetNode
-     */
-    public void focusOn(NodeModel<?> targetNode){
-
-    }
-
-    /**
-     *  un focus on node
-     * @param targetNode targetNode
-     */
-    public void unFocusOn(NodeModel<?> targetNode){
-
-    }
-
-    /**
-     *  for  support scroll view
-     */
-    public void  lockDragDirection(){
-
-    }
-
-    /**
-     *  save  current state
-     */
-    public void saveLastSate(){
-
-    }
-
-    /**
-    *  keep last location and
-     */
-    public void restoreLastSate(){
-
-    }
-
-    /**
-     *  add on select listener
-     */
-    public void addOnSelectedListener(){
-
-    }
-
-    /**
-     *  restructure by dragging;  multi select mode;  single click to focus; totally free drag; add node by node view list
-     * @param status status
-     */
-    public void setEditStatus(int status){
-
     }
 
     /**
@@ -203,14 +81,5 @@ public class TreeViewEditor {
         if(container!=null){
             container.onRemoveChildNodes(parentNode);
         }
-    }
-
-    public interface  TraverseRelationshipCallback{
-       <T> void callback(T root, T parent , T child);
-       default  void callbackView(View rootView, View parentView , View childView){};
-    }
-
-    public interface OnNodeSelectedCallback{
-        <T> void callback(T clickNode, List<T> selectedList);
     }
 }
